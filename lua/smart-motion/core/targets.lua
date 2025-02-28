53670bdc028bfe7d35adc882dcaa851e7de441ec
Keenan <keenanjj13@gmail.com>
2025-02-28 10:50:48 -0700

    feat(targets): created targets module with docs

---
+++ lua/smart-motion/core/targets.lua
@@ -0,0 +1,21 @@
+--- Central dispatcher for retrieving jump targets.
+local word = require("smart-motion.motion.word")
+local consts = require("smart-motion.consts")
+
+local M = {}
+
+--- Fetches jump targets based on type.
+---@param type string "word", "char", "line"
+---@param lines string[] Lines to search.
+---@param direction "before"|"after" Search direction.
+---@param start_line integer Starting line (0-based)
+---@return table[] List of jump targets.
+function M.get_jump_targets(type, ...)
+	if type == consts.TARGET_TYPES.WORD then
+		return word.get_jump_targets_for_word(...)
+	end
+
+	error("Unknown target type: " .. tostring(type))
+end
+
+return M
