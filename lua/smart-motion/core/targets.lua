60367b130c158a7e7b196fd8543db247c4a234f3
Keenan <keenanjj13@gmail.com>
2025-02-28 11:52:25 -0700

    refactor(targets): using new consts

--- lua/smart-motion/core/targets.lua
+++ lua/smart-motion/core/targets.lua
@@ -7,7 +7,7 @@ local M = {}
 --- Fetches jump targets based on type.
 ---@param type string "word", "char", "line"
 ---@param lines string[] Lines to search.
----@param direction "before"|"after" Search direction.
+---@param direction "before_cursor"|"after_cursor" Search direction.
 ---@param start_line integer Starting line (0-based)
 ---@return table[] List of jump targets.
 function M.get_jump_targets(type, ...)
