e555376139bdcc3a27795cf62f7220d979ec30a0
Keenan <keenanjj13@gmail.com>
2025-02-28 11:52:56 -0700

    refactor(lines): using new consts

--- lua/smart-motion/core/lines.lua
+++ lua/smart-motion/core/lines.lua
@@ -6,16 +6,16 @@ local M = {}
 --- Fetches lines based on motion direction.
 ---@param bufnr integer Buffer number.
 ---@param cursor_line integer 0-based cursor line.
----@param direction "before"|"after"
+---@param direction "before_cursor"|"after_cursor"
 ---@return string[] List of lines to scan.
 function M.get_lines_for_motion(bufnr, cursor_line, direction)
 	local last_line = vim.api.nvim_buf_line_count(bufnr)
 
-	if direction == consts.DIRECTION.AFTER then
+	if direction == consts.DIRECTION.AFTER_CURSOR then
 		local max_lines = math.min(state.max_lines, last_line - cursor_line + 1)
 
 		return vim.api.nvim_buf_get_lines(bufnr, cursor_line, cursor_line + max_lines, false)
-	elseif direction == consts.DIRECTION.BEFORE then
+	elseif direction == consts.DIRECTION.BEFORE_CURSOR then
 		local start_line = math.max(cursor_line - state.max_lines, 0)
 
 		return vim.api.nvim_buf_get_lines(bufnr, start_line, cursor_line + 1, false)
