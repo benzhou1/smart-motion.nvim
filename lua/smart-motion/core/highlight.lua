0b1f6911b945ba05c49f9f87319d255631ac42f3
Keenan <keenanjj13@gmail.com>
2025-03-03 10:42:37 -0700

    fix(highlight): fixes issue #2

--- lua/smart-motion/core/highlight.lua
+++ lua/smart-motion/core/highlight.lua
@@ -44,15 +44,15 @@ function M.apply_double_hint_label(ctx, cfg, motion_state, line, col, label, opt
 	local first_char, second_char = label:sub(1, 1), label:sub(2, 2)
 
 	local first_hl = options.dim_first_char and (cfg.highlight.first_char_dim or "SmartMotionFirstCharDim")
-		or (cfg.highlight.first_char or "SmartMotionFirstChar")
+			or (cfg.highlight.first_char or "SmartMotionFirstChar")
 	local second_hl = options.dim_first_char and (cfg.highlight.first_char or "SmartMotionFirstChar")
-		or (cfg.highlight.second_char or "SmartMotionSecondChar")
+			or (cfg.highlight.second_char or "SmartMotionSecondChar")
 
 	log.debug(string.format("Applying double hint '%s%s' at line %d, col %d", first_char, second_char, line, col))
 
 	vim.api.nvim_buf_set_extmark(ctx.bufnr, consts.ns_id, line, col, {
 		virt_text = {
-			{ first_char, first_hl },
+			{ first_char,  first_hl },
 			{ second_char, second_hl },
 		},
 		virt_text_pos = "overlay",
@@ -89,8 +89,19 @@ function M.apply_hint_labels(ctx, cfg, motion_state)
 	)
 
 	for target, label in pairs(motion_state.assigned_hint_labels) do
+		log.debug(
+			string.format(
+				"Target: text=%s line=%d start_pos=%s end_pos=%s label=%s",
+				target.word,
+				target.line,
+				target.start_pos,
+				target.end_pos,
+				label
+			)
+		)
+
 		local pos = (motion_state.hint_position == consts.HINT_POSITION.START) and target.start_pos
-			or (target.end_pos - 1)
+				or (target.end_pos - 1)
 		if #label == 1 then
 			M.apply_single_hint_label(ctx, cfg, motion_state, target.line, pos, label)
 		elseif #label == 2 then
@@ -115,15 +126,10 @@ function M.filter_double_hints(ctx, cfg, motion_state, active_prefix, hints)
 
 	for target, label in pairs(hints) do
 		if label:sub(1, 1) == active_prefix then
-			M.apply_double_hint_label(
-				ctx,
-				cfg,
-				motion_state,
-				target.line,
-				target.start_pos,
-				label,
-				{ dim_first_char = true }
-			)
+			local hint_pos = (motion_state.hint_position == consts.HINT_POSITION.START) and target.start_pos
+					or (target.end_pos - 1)
+
+			M.apply_double_hint_label(ctx, cfg, motion_state, target.line, hint_pos, label, { dim_first_char = true })
 		end
 	end
 end
