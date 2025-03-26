473987f4abb21cb53f25e97788672ca4e729ec58
Keenan <keenanjj13@gmail.com>
2025-03-26 17:43:27 -0600

    feat(targets): added get_target_under_cursor help

--- lua/smart-motion/core/targets.lua
+++ lua/smart-motion/core/targets.lua
@@ -1,3 +1,6 @@
+local consts = require("smart-motion.consts")
+local TARGET_TYPES = consts.TARGET_TYPES
+
 local M = {}
 
 --- Formats a jump target to ensure consistent structure.
@@ -55,6 +58,53 @@ function M.get_jump_targets(ctx, cfg, motion_state, extractor)
 
 	motion_state.jump_targets = jump_targets
 	motion_state.selected_jump_target = first_jump_target
+end
+
+function M.get_target_under_cursor(ctx, cfg, motion_state)
+	local bufid = ctx.bufid
+	local cursor_line, cursor_col = ctx.cursor_line, ctx.cursor_col
+	local line_content = vim.api.nvim_buf_get_lines(bufid, cursor_line, cursor_line + 1, false)[1]
+
+	if not line_content then
+		return nil
+	end
+
+	if motion_state.target_type == TARGET_TYPES.LINES then
+		return M.format_jump_target(ctx, cfg, motion_state, {
+			row = cursor_line,
+			col = 0,
+			start_pos = { row = cursor_line, col = 0 },
+			end_pos = { row = cursor_line, #line_content },
+			text = line_content,
+			type = motion_state.target_type,
+		})
+	elseif motion_state.target_type == TARGET_TYPES.WORDS then
+		local search_start = 0
+
+		while true do
+			local match_data = vim.fn.matchstrpos(line_content, consts.WORD_PATTERN, search_start)
+			local matched_text, match_start, match_end = match_data[1], match_data[2], match_data[3]
+
+			if match_start == -1 then
+				break
+			end
+
+			if cursor_col >= match_start and cursor_col < match_end then
+				return {
+					row = cursor_line,
+					col = match_start,
+					start_pos = { row = cursor_line, col = match_start },
+					end_pos = { row = cursor_line, col = match_end - 1 },
+					text = matched_text,
+					type = motion_state.target_type,
+				}
+			end
+
+			search_start = match_end
+		end
+	end
+
+	return nil
 end
 
 return M
