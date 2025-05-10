aeefda91858348c9f551f5e96736028ca53fed47
Keenan Johns <39966609+FluxxField@users.noreply.github.com>
2025-05-09 20:35:10 -0600

    refactor(modifiers.init): using module_wrapper

--- lua/smart-motion/modifiers/init.lua
+++ lua/smart-motion/modifiers/init.lua
@@ -1,9 +1,19 @@
+local utils = require("smart-motion.utils")
+local default = require("smart-motion.modifiers.default")
+local weight_distance = require("smart-motion.modifiers.weight_distance")
+
 ---@type SmartMotionRegistry<SmartMotionModifierModuleEntry>
 local modifiers = require("smart-motion.core.registry")("modifiers")
 
 modifiers.register_many({
-	default = require("smart-motion.modifiers.default"),
-	weight_distance = require("smart-motion.modifiers.weight_distance"),
+	default = {
+      run = utils.module_wrapper(default.run),
+      metadata = default.metadata,
+    },
+	weight_distance = {
+      run = utils.module_wrapper(weight_distance.run),
+      metadata = weight_distance.metadata,
+    },
 })
 
 return modifiers
