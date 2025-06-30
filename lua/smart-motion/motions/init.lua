cc033af0694117432eb7fb860c6e269ae8445e8a
Ben Zhou <benzhou29@gmail.com>
2025-06-30 06:09:32 -0500

    feat: Fix deprecated which key register calls

--- lua/smart-motion/motions/init.lua
+++ lua/smart-motion/motions/init.lua
@@ -140,7 +140,7 @@ function motions.map_motion(name, motion_opts, opts)
 	if opts.which_key ~= false and package.loaded["which-key"] then
 		local wk = require("which-key")
 		for _, mode in ipairs(modes) do
-			wk.register({ trigger_key, desc = desc, mode = mode })
+			wk.add({ trigger_key, desc = desc, mode = mode })
 		end
 	end
 
