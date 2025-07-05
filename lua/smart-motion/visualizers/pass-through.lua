8744d96fa73ca0c0e214bade613946ccabc8fb02
Keenan Johns <39966609+FluxxField@users.noreply.github.com>
2025-07-05 13:35:11 -0500

    fear: create pass-through.lua for visualizer

---
+++ lua/smart-motion/visualizers/pass-through.lua
@@ -0,0 +1,15 @@
+local log = require("smart-motion.core.log")
+
+---@type SmartMotionModifierModuleEntry
+local M = {}
+
+function M.run(ctx, cfg, motion_state, target)
+	return target
+end
+
+M.metadata = {
+	label = "Default Passthrough",
+	description = "Yields all targets unchanged",
+}
+
+return M
