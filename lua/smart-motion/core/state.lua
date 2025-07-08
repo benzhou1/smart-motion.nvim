7fca632faa093e09b67eeab389990841845bfbf4
FluxxField <keenanjj13@protonmail.com>
2025-07-08 09:13:13 -0500

    refactor(state): merge motion state now adds motion

--- lua/smart-motion/core/state.lua
+++ lua/smart-motion/core/state.lua
@@ -150,7 +150,12 @@ function M.module_has_motion_state(module)
 	return module.metadata and module.metadata.motion_state
 end
 
-function M.merge_motion_state(motion_state, ...)
+function M.merge_motion_state(motion_state, motion, ...)
+	motion_state = vim.tbl_deep_extend("force", motion_state, motion.metadata.motion_state)
+
+	-- Add the motion data to motion_state so that it is passed around to the modules
+	motion_state.motion = motion
+
 	for _, module in ipairs({ ... }) do
 		if M.module_has_motion_state(module) then
 			motion_state = vim.tbl_deep_extend("force", motion_state, module.metadata.motion_state)
